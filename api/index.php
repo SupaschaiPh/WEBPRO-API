f835f78fceef55c969e073edb3d7a795d5b3d326
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-02-08 21:50:06 +0700

    add:phpmyadmin

--- api/index.php
+++ api/index.php
@@ -3,8 +3,8 @@ header('Content-Type: application/json; charset=utf-8');
 //header('Access-Control-Allow-Origin: *');
 ?>
 <?php
-    echo json_encode(array(
+    print(json_encode(array(
         "status" => 200,
         "msg" => "Ok",
-    ));
+    )));
 ?>
---
+++ api/tool/Readme.md
@@ -0,0 +1,6 @@
+# INFO
+## Q : I Get problem when open phpmyadmin
+run this command on docker exec
+```
+docker-php-ext-install mysqli
+```
