a4f82c240ad12db50b785d4649fc096c0f167909
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-02-14 08:38:32 +0700

    fix:redirect

--- api/bucket/index.php
+++ api/bucket/index.php
@@ -1,14 +1,14 @@
 <?php
 header('Content-Type:file');
 if (array_key_exists("file", $_GET)) {
-    try{
-    $file = fopen("upload/".$_GET["file"], "r");
-    echo fread($file, filesize("upload/".$_GET["file"]));
-    fclose($file);
-    }catch(Exception $e){
+    try {
+        $file = fopen("upload/" . $_GET["file"], "r");
+        echo fread($file, filesize("upload/" . $_GET["file"]));
+        fclose($file);
+    } catch (Exception $e) {
         die("Cannot Read File");
     }
-}else{
- http_response_code(404);
+} else {
+    http_response_code(404);
 }
 //die("Permission denine");
