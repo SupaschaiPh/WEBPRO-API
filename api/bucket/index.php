9c4307e45ee7a347407d2552e81a94057efd20f3
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-02-14 08:23:24 +0700

    feat(apache):protect upload file

--- api/bucket/index.php
+++ api/bucket/index.php
@@ -1,8 +1,14 @@
 <?php
 header('Content-Type:file');
-
-$file = fopen("./test.txt","r");
-echo fread($file,filesize("./test.txt"));
-fclose($file);
+if (array_key_exists("file", $_GET)) {
+    try{
+    $file = fopen("upload/".$_GET["file"], "r");
+    echo fread($file, filesize("upload/".$_GET["file"]));
+    fclose($file);
+    }catch(Exception $e){
+        die("Cannot Read File");
+    }
+}else{
+ http_response_code(404);
+}
 //die("Permission denine");
-?>
