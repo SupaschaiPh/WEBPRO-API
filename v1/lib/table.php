f3ef554793e3f729c8db2c3e8eaf1e586a70c64c
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-03-04 00:32:55 +0700

    fix:get table wrong limit

--- v1/lib/table.php
+++ v1/lib/table.php
@@ -17,7 +17,7 @@ function getTables($limit = null, $offset = 0, $filters = null)
         $sql = "SELECT * FROM table_info LEFT OUTER JOIN table_order USING (table_id) LEFT OUTER JOIN user ON table_order.receive_id=user.id ". filterObjToSQL($conn, $filters) . ";";
     }
     $res = mysqli_fetch_all(mysqli_query($conn, $sql), MYSQLI_ASSOC);
-    $maximumlimit = mysqli_fetch_all(mysqli_query($conn, "SELECT count(table_id) FROM table_info ". filterObjToSQL($conn, $filters) . " ;"));
+    $maximumlimit = mysqli_fetch_all(mysqli_query($conn, "SELECT count(table_id) FROM table_info  LEFT OUTER JOIN table_order USING (table_id) ". filterObjToSQL($conn, $filters) . " ;"));
     $hold["data"] = $res;
     $hold["limit"] = $maximumlimit[0][0];
     mysqli_close($conn);
