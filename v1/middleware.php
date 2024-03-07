b0934fc36bb1f54c92f09ec74cefe35ebf45a970
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-03-07 20:51:09 +0700

    feat:generate payment link

--- v1/middleware.php
+++ v1/middleware.php
@@ -1,7 +1,9 @@
 <?php
 define("CONFIG", parse_ini_file(__DIR__ . "/../config.ini"));
 
-session_start();
+if (session_status() != PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
 define(
     "GET_ALLOW",
@@ -83,76 +85,75 @@ define(
         "/v1/table/order/add/",
         "/v1/order/edit/",
         "/v1/order/transaction/accept/",
-        "/v1/payment/genqr/"
+        "/v1/payment/genqr/",
+        "/v1/payment/genlink/"
+
     )
 );
 
 define(
     "STAFF_ROUTE",
-    array(
-        
-    )
+    array()
 );
-$json = file_get_contents("php://input",true);
+$json = file_get_contents("php://input", true);
 $json = json_decode($json);
 //$_SERVER["REQUEST_URI"] 
 //echo json_encode($_POST);
 
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), GET_ALLOW)) {
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), GET_ALLOW)) {
     if (strcmp($_SERVER["REQUEST_METHOD"], "GET") != 0) {
         http_response_code(405);
         die();
     }
-    if($json)
-    foreach ($json as $key => $value) {
-       $_GET[$key] = $value;
-    }
+    if ($json)
+        foreach ($json as $key => $value) {
+            $_GET[$key] = $value;
+        }
 }
 
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), POST_ALLOW)) {
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), POST_ALLOW)) {
     if (strcmp($_SERVER["REQUEST_METHOD"], "POST") != 0) {
         http_response_code(405);
         die();
     }
-    if($json)
-    foreach ($json as $key => $value) {
-        $_POST[$key] = $value;
-     }
-} 
+    if ($json)
+        foreach ($json as $key => $value) {
+            $_POST[$key] = $value;
+        }
+}
 
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), DELETE_ALLOW)) {
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), DELETE_ALLOW)) {
     if (!(strcmp($_SERVER["REQUEST_METHOD"], "DELETE") == 0 || strcmp($_SERVER["REQUEST_METHOD"], "POST") == 0)) {
         http_response_code(405);
         die();
     }
-    if($json)
-    foreach ($json as $key => $value) {
-        $_POST[$key] = $value;
-        $_DELETE[$key] = $value;
+    if ($json)
+        foreach ($json as $key => $value) {
+            $_POST[$key] = $value;
+            $_DELETE[$key] = $value;
+        }
+}
 
-     }
-} 
-
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), PUT_ALLOW)) {
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), PUT_ALLOW)) {
     if (!(strcmp($_SERVER["REQUEST_METHOD"], "PUT") == 0 || strcmp($_SERVER["REQUEST_METHOD"], "POST") == 0)) {
         http_response_code(405);
         die();
     }
-    if($json)
-    foreach ($json as $key => $value) {
-        $_POST[$key] = $value;
-        $_PUT[$key] = $value;
-     }
-} 
+    if ($json)
+        foreach ($json as $key => $value) {
+            $_POST[$key] = $value;
+            $_PUT[$key] = $value;
+        }
+}
 
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), AUTH_ROUTE)) {
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), AUTH_ROUTE)) {
     if (!(isset($_SESSION["uinfo"]) && isset($_SESSION["uinfo"]["id"]))) {
         http_response_code(401);
         die();
     }
-} 
-if (in_array(str_replace("index.php","",$_SERVER["SCRIPT_NAME"]), STAFF_ROUTE)) {
-    if (!(isset($_SESSION["uinfo"]) && isset($_SESSION["uinfo"]["role"]) && strcmp($_SESSION["uinfo"]["role"],"staff") == 0)) {
+}
+if (in_array(str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]), STAFF_ROUTE)) {
+    if (!(isset($_SESSION["uinfo"]) && isset($_SESSION["uinfo"]["role"]) && strcmp($_SESSION["uinfo"]["role"], "staff") == 0)) {
         http_response_code(403);
         die();
     }
---
+++ v1/payment/genlink/index.php
@@ -0,0 +1,45 @@
+<?php
+
+include "../../hearder.php";
+include "../../generalFn.php";
+include_once "../../middleware.php";
+include_once "../../lib/employee.php";
+try{
+$checkSum = null;
+$bill_id = 0;
+if (isset($_SESSION["uinfo"]) && isset($_SESSION["uinfo"]["id"])) {
+    if (isset($_SESSION["latest_insert_bill_id"])) {
+        $bill_id = $_SESSION["latest_insert_bill_id"];
+    }
+    $employeeCheck = getEmployees(1, 0, json_encode(array(array(
+        "key" => "id",
+        "type" => "equal",
+        "filter" => $_SESSION["uinfo"]["id"] . ""
+    ))));
+    if ($employeeCheck && isset($employeeCheck["data"][0]["e_name"])) {
+        $sa = $employeeCheck["data"][0]["e_name"] . $employeeCheck["data"][0]["e_lastname"] . $employeeCheck["data"][0]["start_date"] . $bill_id;
+        $sa = $_SESSION["uinfo"]["id"] . $sa;
+        $checkSum = password_hash($sa, PASSWORD_DEFAULT);
+    }
+}
+$link = ("http://" . $_SERVER["HTTP_HOST"] . "/v1/payment/pay/?bill_id=" . $bill_id . "&pay_to=" . $_SESSION["uinfo"]["id"] . "&checksum=" . $checkSum);
+//$qrUrl = "https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=".$link;
+
+//echo file_get_contents($qrUrl);
+//echo "<img src='".$qrUrl."' />";
+//echo "<a href=".urldecode($link).">".urldecode($link)."</a>";
+//echo  "<p>".$sa."</p>";
+echo json_encode(
+    array(
+        "status" =>"success",
+        "link" => urldecode($link)
+    )
+);
+}catch (\Throwable $th){
+    echo json_encode(
+        array(
+            "status" =>"fail",
+            "status" =>"you are not employee or something went wrong"
+        )
+    );
+}
