73d29eac479a7f9f0f8b7244f59a4556503d7e36
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-03-11 16:23:26 +0700

    fix:cors

--- v1/hearder.php
+++ v1/hearder.php
@@ -2,7 +2,11 @@
 header('Content-Type: application/json; charset=utf-8',true,200);
 //header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 define("FRONTEND_URL", parse_ini_file(__DIR__ . "/../config.ini")["FRONTEND_URL"]);
+
 header('Access-Control-Allow-Origin: '.FRONTEND_URL,true); 
+if(isset($_SERVER["HTTP_ORIGIN"])){
+    header('Access-Control-Allow-Origin: '.$_SERVER["HTTP_ORIGIN"],true); 
+}
 //header('Access-Control-Allow-Origin: * '); 
 //header("Referrer-Policy: no-referrer");
 header('Access-Control-Allow-Credentials: true'); 
